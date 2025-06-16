e6ad8f784e0265e760635f1a689397ff65c6b960
Caio Oliveira <xcaio2@gmail.com>
2025-06-16 19:49:05 +0100

    services full responsive

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -3,11 +3,12 @@ import ServicesGrid from "@/app/components/ServicesGrid";
 
 export function Services() {
     return (
-        <section id="services" className="grid-cols-1 md:grid-cols-2 w-[90vw] mt-[8rem] m-auto">
-            <div className="gap-[4rem]">
-                <div className="flex bg-[#646DD2] rounded-md items-center px-4 py-2 mb-[1em] ">
-                    <img className="w-40 h-40 object-cover rounded-full ml-auto mr-5" src="../../images/handshake.jpg" alt="profile-picture"/>
-                    <p className="text-[1.8rem] text-white font-bold ">
+        <section id="services" className="grid-cols-1 md:grid-cols-2 w-[90vw] mt-[8rem] m-auto md:w-[70vw]">
+            <div className="gap-[4rem] md:mb-[10rem]">
+                <div className="flex bg-[#646DD2] rounded-md items-center px-4 py-2 mb-[1em]
+                md:flex-row md:w-[50vw] md:rounded-4xl md:mt-[15rem] md:mx-auto">
+                    <img className="w-40 h-40 object-cover rounded-full ml-auto mr-5 md:mr-8" src="../../images/handshake.jpg" alt="profile-picture"/>
+                    <p className="text-[1.8rem] text-white font-bold md:text-[4rem] md:mr-auto">
                         Services
                     </p>
                 </div>
--- app/components/ServicesGrid.tsx
+++ app/components/ServicesGrid.tsx
@@ -6,33 +6,40 @@ import React from "react";
 
 export default function ServicesGrid() {
     return (
-        <div className="flex flex-col px-4">
+        <div className="flex flex-col px-4 md:w-[50vw] md:m-auto md:overflow-x-visible">
 
             <div className="relative grid-cols-1 flex flex-col gap-7">
                 <img alt="bg-img-1" src="../../images/background/yellow_and_purple_b.png"
-                     className="absolute -z-5 top-0 left-0 w-auto h-auto min-w-full min-h-full object-cover scale-150 -mt-[8rem] "
+                     className="absolute -z-5  w-auto h-auto min-w-full min-h-full object-cover scale-150 -mt-[8rem]
+                      md:object-fit md:scale-160 md:min-w-0 md:min-h-0 md:gap-3"
                 />
-                <ServiceCard
-                    title="Website"
-                    description="Building custom websites with a focus on user experience, responsive design, and modern technologies."
-                    Icon={TbWorld}
-                />
-                <ServiceCard
-                    title="Branding"
-                    description="Creating cohesive brand experiences across all touch-points, from logo design and website development to marketing materials and social media presence."
-                    Icon={PiLightbulbBold}
-                />
+                <div className="gap-7 flex flex-col md:flex-row md:gap-3">
+                    <ServiceCard
+                        title="Website"
+                        description="Building custom websites with a focus on user experience, responsive design, and modern technologies."
+                        Icon={TbWorld}
+                    />
+                    <ServiceCard
+                        title="Branding"
+                        description="Creating cohesive brand experiences across all touch-points, from logo design and website development to marketing materials and social media presence."
+                        Icon={PiLightbulbBold}
+                        marginTop="md:-mt-[5rem]"
+                    />
+                </div>
 
-                <ServiceCard
-                    title="E-commerce"
-                    description="Developing robust online stores with features like product catalogs, shopping carts, secure payment processing, and order management."
-                    Icon={LuSmartphone}
-                />
-                <ServiceCard
-                    title="SEO"
-                    description="Optimizing websites for local search results to attract customers in specific geographic areas."
-                    Icon={FaGoogle}
-                />
+            <div className="gap-7 flex flex-col md:flex-row md:gap-3">
+                    <ServiceCard
+                        title="E-commerce"
+                        description="Developing robust online stores with features like product catalogs, shopping carts, secure payment processing, and order management."
+                        Icon={LuSmartphone}
+                    />
+                    <ServiceCard
+                        title="SEO"
+                        description="Optimizing websites for local search results to attract customers in specific geographic areas."
+                        Icon={FaGoogle}
+                        marginTop="md:-mt-[5rem]"
+                    />
+            </div>
             </div>
         </div>
     );
@@ -42,11 +49,15 @@ type ServiceCardProps = {
     title: string;
     description: string;
     Icon: React.ElementType;
+    marginTop?: string;
 };
 
-function ServiceCard({ title, description, Icon }: ServiceCardProps) {
+function ServiceCard({ title, description, Icon, marginTop }: ServiceCardProps) {
     return (
-        <div className="relative group flex flex-col justify-between items-start h-[15em] px-8 pt-10 pb-4 border-4 border-white rounded-2xl overflow-hidden transition-all duration-300 ease-in-out hover:border-[#646DD2] hover:shadow-[-2px_8px_8px_rgba(77,76,76,0.4)]">
+        <div className={`relative group flex flex-col justify-between items-start h-[15em] px-8 pt-10 pb-4 border-4 border-white rounded-2xl
+        overflow-hidden transition-all duration-300 ease-in-out hover:border-[#646DD2] hover:shadow-[-2px_8px_8px_rgba(77,76,76,0.4)]
+        md:w-[50%] ${marginTop} md:rounded-4xl
+        `}>
             <div className="flex flex-col justify-around h-[60%] ">
                 <h2 className="text-white font-bold text-4xl mb-1">{title}</h2>
 
@@ -59,8 +70,10 @@ function ServiceCard({ title, description, Icon }: ServiceCardProps) {
                 </button>
             </div>
 
-            <div className="absolute bottom-[-0.3rem] right-[-1.5rem] w-[60%] aspect-square border border-gray-400 rounded-xl flex justify-center items-center rotate-[43deg] transition-all duration-500 ease-in-out group-hover:bg-[#646DD2] z-0">
-                <Icon className="w-[60%] h-[60%] text-white -rotate-[90deg] mt-8 transition-all m-auto duration-500 ease-in-out group-hover:text-[#C9AA71]" />
+            <div className="absolute bottom-[-0.3rem] right-[-1.5rem] w-[60%] aspect-square border border-gray-400
+            md:bottom-[-3rem] md:right-[-3rem]
+             rounded-xl flex justify-center items-center rotate-[43deg] transition-all duration-500 ease-in-out group-hover:bg-[#646DD2] z-0">
+                <Icon className="top-2 right-3 absolute md:top-5 md:right-5 left-0 bottom-0 w-[60%] h-[60%] text-white -rotate-[90deg] mt-8 transition-all m-auto duration-500 ease-in-out group-hover:text-[#C9AA71]" />
             </div>
         </div>
     );
