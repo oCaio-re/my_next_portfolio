cacb5225e75dfc2bef184176029cd403729be243
Caio Oliveira <xcaio2@gmail.com>
2025-06-17 11:56:05 +0100

    home medium ok

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -11,16 +11,16 @@ export function Home() {
         }
     };
     return (
-        <section id="home" className="relative bg-[#646DD2] h-[100vh] md:min-h-[80vh] md:h-[80vh]">
+        <section id="home" className="relative bg-[#646DD2] h-[100vh] md:min-h-[80vh] md:h-[100vh] lg:h-[80vh] scroll-mt-32">
             <img src="../../images/wave%20(2).svg" alt="wave-svg"
                  className="-scale-y-100 overflow-hidden absolute
                   z-5 top-0 left-0 w-auto h-auto min-w-full min-h-full object-cover scale-100" />
             <div className="top-0 -scale-y-100 object-cover w-[200rem] overflow-hidden "/>
-            <div className="relative grid-cols-1 md:grid-cols-2 w-[95vw] mt-[10rem] mx-auto flex flex-col z-50 drop-shadow-xl
-                    md:w-[70vw] md:flex-row  md:justify-between md:items-center
+            <div className="relative grid-cols-1 md:flex-col lg:flex-row w-[95vw] mt-[10rem] mx-auto flex flex-col z-50 drop-shadow-xl
+                    md:w-[70vw]  md:justify-between md:items-center md:mt-[6rem] lg:mt-[10rem]
             ">
                 <div className="md:m-auto md:ml-0 md:text-[3rem] md:justify-start flex flex-col ">
-                    <div className="flex items-center gap-2 ">
+                    <div className="flex items-center gap-2">
                         <img className="w-25 h-25 object-cover rounded-full" src="../../images/caio-profile-half.jpeg" alt="profile-picture"/>
                         <div className="font-bold bg-white rounded-full py-3 px-3">
                             Hello, I&#39;m Caio!
@@ -38,10 +38,10 @@ export function Home() {
                 </div>
                     <div className="mt-[5rem] h-[25rem] relative flex">
                         <div className="absolute m-auto left-0 right-0 bg-white rounded-full h-[20rem] w-[20rem] z-1
-                            md:h-[25rem] md:w-[25rem]
+                            md:h-[20rem] lg:h-[25rem] md:w-[20rem] lg:w-[25rem]
                         "/>
                         <img alt="smiling-man" src="../../images/smiling-man.png "
-                              className="z-50 relative w-[80vw] m-auto md:w-[25vw]"/>
+                              className="z-50 relative w-[80vw] m-auto md:w-[35vw] lg:w-[25vw]"/>
                     </div>
             </div>
 
